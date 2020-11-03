a4f053d7fe61b71941418b9bfb5a1cfee322cecc
pvsource <pavelsergmgtu@gmail.com>
2020-11-03 17:22:16 +0500

    Всё тоже самое тольько после tsc

--- build_es6_strict/FileSystem.js
+++ build_es6_strict/FileSystem.js
@@ -2,8 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const Folder_1 = require("./Folder");
 class FileSystem extends Folder_1.default {
-    constructor() {
+    constructor(name) {
         super(name);
+    }
+    clone() {
+        let clone_fs = new FileSystem(this.name);
+        Object.assign(clone_fs.composition, this.composition);
+        return clone_fs;
     }
 }
 //# sourceMappingURL=FileSystem.js.map
